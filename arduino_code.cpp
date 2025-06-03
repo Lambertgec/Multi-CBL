da33f32dcfded6fbdafe3f3628f72cf6b88933e0
Lambertgec <keszthelyi.boldi9@gmail.com>
2025-06-03 10:23:47 +0200

    Added constants for Light and Buzzer pins, enabled them both once posture was stable for testing

--- arduino_code.cpp
+++ arduino_code.cpp
@@ -5,6 +5,8 @@
 // Variables to change manually depending on setup
 const int pinsUsed = 4; //number of analog pins used
 const int analogPins[pinsUsed] = {A0, A1, A2, A3}; //analog pins to read (for sensors)
+const int LED_PIN = 13; //pin for the LED, as its RGB it will also probably have 3 pins?
+const int buzzerPins[pinsUsed] = {2, 3, 4, 5}; //pins for the buzzers (one for each sensor)
 
 // Variables we have to play around with
 const int window_size = 20; //size of the rolling window for each sensor
@@ -50,7 +52,10 @@ float mad(float arr[], int size, float med) { //returns the Median Absolute Devi
 
 void setup() {
   Serial.begin(115200); //this has to be the same as in platformio.ini OR in the serial monitor
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
   for (int i = 0; i < pinsUsed; i++)
+    pinMode(analogPins[i], INPUT); // Optional for clarity
+    pinMode(buzzerPins[i], OUTPUT); // Set buzzer pins as output
     for (int j = 0; j < window_size; j++)
       datas[i][j] = 0.0; //initialize rolling windows to 0.0
   last_unstable_time = millis(); //initialize last unstable time
@@ -123,10 +128,15 @@ void loop() {
 
   if (debounced) { // if all sensors were consistent for the last debounce_length readings
     if (millis() - last_unstable_time > stability_seconds) { //if enough time elapsed and we didnt yet report posture as stable, this is where we light up the LED and make the buzzers pulse
-      Serial.print("Stable posture detected for ");
-      Serial.print(stability_seconds / 1000);
-      Serial.println(" seconds!");
-      stable_reported = true;
+        char buf[64];
+        snprintf(buf, sizeof(buf), "Stable posture detected for %.2f seconds! \n", stability_seconds / 1000.0);
+        Serial.println(buf);
+        digitalWrite(LED_PIN, HIGH); // Turn on the LED, if it works this should be changed to a method
+        digitalWrite(buzzerPins[0], HIGH); // Turn on the first buzzer, if it works this should be changed to a method
+        // Serial.print("Stable posture detected for "); //kept in case snprintf doesnt work
+        // Serial.print(stability_seconds / 1000);
+        // Serial.println(" seconds!");
+        stable_reported = true; // we probably don't need this
     }
   } else { // there was atleast one sensor having inconsistent readings hence we reset the timer
     last_unstable_time = millis();
